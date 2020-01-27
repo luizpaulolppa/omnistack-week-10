d02eead82c40280efa7b7cd9158b4ce5c6ed81a6
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2020-01-26 23:35:29 -0300

    create app web a add new lib cors

--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+// UTILS:
+// Componente: Bloco isolado de HTML, CSS e JS, o quel não interfere no restante da aplicação.
+// Estado: Informações que um componente PAI passa para o componente FILHO.
+// Propriedade: informação mantidas pelo componente.
